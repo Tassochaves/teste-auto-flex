06756689ff4f564f7d1978d4077c35d34b4aeff5
Tassochaves <tassogt@hotmail.com>
2026-02-17 23:55:40 -0300

    first commit back-end

---
+++ backend/autoflex-back/src/main/java/com/dev/autoflex_back/AutoflexBackApplication.java
@@ -0,0 +1,13 @@
+package com.dev.autoflex_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoflexBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoflexBackApplication.class, args);
+	}
+
+}
---
+++ backend/autoflex-back/src/test/java/com/dev/autoflex_back/AutoflexBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.autoflex_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoflexBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
