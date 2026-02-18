4af51eaf655cb196fc60648246a3158e27b9b394
Tassochaves <tassogt@hotmail.com>
2026-02-18 12:05:09 -0300

    feat(backend): implement product creation with code validation

---
+++ backend/autoflex-back/src/main/java/com/dev/autoflex_back/controller/ProductController.java
@@ -0,0 +1,31 @@
+package com.dev.autoflex_back.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.dev.autoflex_back.dto.request.ProductRequest;
+import com.dev.autoflex_back.dto.response.ProductResponse;
+import com.dev.autoflex_back.service.ProductService;
+
+@RestController
+@RequestMapping("/api/v1/products")
+public class ProductController {
+
+    private final ProductService productService;
+
+    public ProductController(ProductService productService){
+        this.productService = productService;
+    }
+
+    @PostMapping
+    public ResponseEntity<ProductResponse> createProduct(@RequestBody ProductRequest request){
+
+        ProductResponse response = productService.create(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+}
--- backend/autoflex-back/src/main/java/com/dev/autoflex_back/serviceImpl/ProductServiceImpl.java
+++ backend/autoflex-back/src/main/java/com/dev/autoflex_back/serviceImpl/ProductServiceImpl.java
@@ -1,15 +1,22 @@
 package com.dev.autoflex_back.serviceImpl;
 
 import java.util.List;
-
 import org.springframework.stereotype.Service;
 
 import com.dev.autoflex_back.dto.request.ProductRequest;
 import com.dev.autoflex_back.dto.response.ProductResponse;
+import com.dev.autoflex_back.entity.Product;
+import com.dev.autoflex_back.repository.ProductRepository;
 import com.dev.autoflex_back.service.ProductService;
 
 @Service
 public class ProductServiceImpl implements ProductService{
+
+    private final ProductRepository productRepository;
+
+    public ProductServiceImpl(ProductRepository productRepository){
+        this.productRepository = productRepository;
+    }
 
     @Override
     public List<ProductResponse> findAllSortedByPrice() {
@@ -24,7 +31,17 @@ public class ProductServiceImpl implements ProductService{
 
     @Override
     public ProductResponse create(ProductRequest request) {
-        return null;
+        
+        if (productRepository.existsByCodeIgnoreCase(request.code())){
+            throw new RuntimeException("Product code already exists: " + request.code());
+        }
+
+        Product product = new Product();
+        product.setCode(request.code());
+        product.setName(request.name());
+        product.setPrice(request.price());
+
+        return ProductResponse.fromEntity(productRepository.save(product));
     }
 
     @Override
