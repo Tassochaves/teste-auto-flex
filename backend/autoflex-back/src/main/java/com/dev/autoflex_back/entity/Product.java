58bb6c8782271debb7eb238cb998e72c502cc085
Tassochaves <tassogt@hotmail.com>
2026-02-18 09:30:29 -0300

    feat(backend): creates a product entity and configures the H2 database.

---
+++ backend/autoflex-back/src/main/java/com/dev/autoflex_back/entity/Product.java
@@ -0,0 +1,65 @@
+package com.dev.autoflex_back.entity;
+
+import java.math.BigDecimal;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Digits;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
+@Entity
+@Table(name = "products")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Code is required")
+    @Column(nullable = false)
+    private String code;
+
+    @NotBlank(message = "Product name is required")
+    @Column(nullable = false)
+    private String name;
+
+    @NotNull(message = "Price is required")
+    @Positive(message = "Price must be greater than zero")
+    @Digits(integer = 13, fraction = 2)
+    @Column(nullable = false, precision = 15, scale = 2)
+    private BigDecimal price;
+
+    public Product(){}
+
+    //Getters and Setter
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getCode() {
+        return code;
+    }
+    public void setCode(String code) {
+        this.code = code;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public BigDecimal getPrice() {
+        return price;
+    }
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+}
